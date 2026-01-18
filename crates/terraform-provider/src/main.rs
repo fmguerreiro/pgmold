e8277f280e8abecf0d3e7ba69356b93f13c76628
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2026-01-18 11:04:41 +0900

    Remove terraform-provider scaffolding in favor of separate repo. (#13)

--- crates/terraform-provider/src/main.rs
+++
@@ -1,7 +0,0 @@
-use terraform_provider_pgmold::PgmoldProvider;
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> anyhow::Result<()> {
-    tf_provider::serve("pgmold", PgmoldProvider::default()).await?;
-    Ok(())
-}
