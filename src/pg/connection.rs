e87a44bdabd67247ebaa9e07e2bba9a9f3767052
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2026-02-10 11:24:00 +0900

    fixup: fmt

--- src/pg/connection.rs
+++ src/pg/connection.rs
@@ -13,8 +13,7 @@ impl PgConnection {
             .connect(connection_string)
             .await
             .map_err(|e| {
-                let sanitized_error =
-                    sanitize_connection_error(connection_string, &e.to_string());
+                let sanitized_error = sanitize_connection_error(connection_string, &e.to_string());
                 SchemaError::DatabaseError(format!(
                     "Failed to connect to {}: {sanitized_error}",
                     sanitize_url(connection_string)
